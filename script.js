13b2868bbd4d7f22fff24b81d849607ead10d320
bnelson <bnelsonemail@icloud.com>
2024-05-25 02:10:14 -0400

    added a desired conversion drop down, added problem and steps to the JS

---
+++ script.js
@@ -0,0 +1,22 @@
+/*          This is a simple script that will be used to demonstrate the use of the temperature converter class.
+
+The script will create an instance of the TemperatureConverter class and use it to convert temperatures between Fahrenheit, Celsius, and Kelvin.
+
+The html file will accept a temperature input and a select dropdown to choose the unit of the input temperature. The user will then be able to select the unit they would like to convert the temperature to and click a button to perform the conversion.
+
+The result of the conversion will be displayed on the page.
+
+The script will also include a function to validate the input temperature and display an error message if the input is not a valid number.
+
+Steps:
+
+1. Create an instance of the TemperatureConverter class.
+2. Create a function to validate the input temperature.
+3. Create a function to convert the temperature.
+4. Create a function to display the result of the conversion.
+5. Add event listener to the button to perform the conversion.
+6. Display the result on the page.
+
+*/
+
+// Solution:
