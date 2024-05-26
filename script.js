882c5c7df94a79f8497ae967e58b5fe7b0104a5b
bnelson <bnelsonemail@icloud.com>
2024-05-26 02:23:34 -0400

    added text animation to the header.

--- script.js
+++ script.js
@@ -40,6 +40,25 @@ Steps:
 
 // Solution:
 
+// Add Heading Letter Animation
+function randomRGB() {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+}
+
+
+// Change the color of the letters every second
+const letters = document.querySelectorAll('.letters');
+const interValid = setInterval(function(){
+    for (let letter of letters) {
+        letter.style.color = randomRGB();
+    }
+}, 1000);
+
+
+
 // Create a class called TemperatureConverter.
 class TemperatureConverter {
   constructor() {
